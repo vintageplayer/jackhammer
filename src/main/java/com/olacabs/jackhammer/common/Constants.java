3f0501e949f8601805397115835a6148682dfa4c
Aditya Agarwal <adityag2511@gmail.com>
2020-04-09 19:23:39 +0530

    BugFix: Adding missing bitbucket url

--- src/main/java/com/olacabs/jackhammer/common/Constants.java
+++ src/main/java/com/olacabs/jackhammer/common/Constants.java
@@ -260,6 +260,7 @@ public class Constants {
     public static  final String JACKHAMMER = "Jackhammer";
     public static  final String GITHUB_API_URL = "https://api.github.com/";
     public static  final String BITBUCKET_API_URL = "https://api.bitbucket.org";
+    public static  final String BITBUCKET_URL = "https://bitbucket.org";
     public static  final String ACCESS_TOKEN = "access_token";
     public static  final String GRANT_TYPE = "grant_type";
     public static  final String CLIENT_CREDENTIALS = "client_credentials";
